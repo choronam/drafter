ac0bc76f6e9d7b12d11e7d834d52742b7ec3c090
Thomas Jandecka <thomas.jandecka@oracle.com>
2019-04-10 13:02:49 +0200

    fix: ci ctest scripts

--- cmake/unix_multi.cmake
+++ cmake/unix_multi.cmake
@@ -22,18 +22,22 @@ ctest_configure(BUILD ${CTEST_BINARY_DIRECTORY} RETURN_VALUE ret_con)
 ctest_build(BUILD ${CTEST_BINARY_DIRECTORY} RETURN_VALUE ret_bld)
 
 if(ret_bld EQUAL 0)
-    # add as desired
     ctest_test(BUILD ${CTEST_BINARY_DIRECTORY} RETURN_VALUE ret_tst)
+endif()
 
-    if(NOT ret_tst EQUAL 0)
-        message(FATAL_ERROR "CI failing on tests")
-    endif()
-else()
+ctest_submit(RETURN_VALUE ret_sub)
+
+if(NOT ret_con EQUAL 0)
+    message(FATAL_ERROR "CI failing on config")
+endif()
+
+if(NOT ret_bld EQUAL 0)
     message(FATAL_ERROR "CI failing on build")
 endif()
 
-# standard submit
-ctest_submit(RETURN_VALUE ret_sub)
+if(NOT ret_tst EQUAL 0)
+    message(FATAL_ERROR "CI failing on tests")
+endif()
 
 if(NOT ret_sub EQUAL 0)
     message(WARNING "Unable to submit results to CDash")
